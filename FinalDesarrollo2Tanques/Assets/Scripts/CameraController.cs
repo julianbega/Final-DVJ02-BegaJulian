77c7e401067e633e1048beb387ae2647eacbe5f0
Julian Bega <julian.begamilrad@gmail.com>
2021-07-30 17:18:25 -0300

    Camera controller

---
+++ FinalDesarrollo2Tanques/Assets/Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraController : MonoBehaviour
+{
+    public Transform lookAtThat;
+
+    [SerializeField] [Range(1, 10)] public float verticalDistance;
+    [SerializeField] [Range(2, 8)] public float horizontalDistanceX;
+    [SerializeField] [Range(-3, 3)] public float horizontalDistanceZ;
+    private Vector3 zoom;
+
+    private Vector3 posToMoveTowards;
+
+    private void Start()
+    {
+        LookAtPlayer();
+    }
+
+    private void OnDisable()
+    {
+    }
+    void LateUpdate()
+    {        
+        MoveCameraToFolowTarget();
+
+        if (this.transform.rotation.x > 70 || this.transform.rotation.y < 75 || this.transform.rotation.y > 103)
+        {
+            LookAtPlayer();
+        }
+    }
+    public void LookAtPlayer()
+    {
+        Debug.Log("look at player");
+        transform.LookAt(lookAtThat.transform);
+    }
+    public void MoveCameraToFolowTarget()
+    {
+        Vector3 myPos = transform.position;
+
+        zoom = new Vector3(-horizontalDistanceX, verticalDistance, horizontalDistanceZ);
+
+        posToMoveTowards = lookAtThat.transform.position + zoom;
+
+        transform.position = Vector3.Lerp(myPos, posToMoveTowards, Vector3.Distance(myPos, posToMoveTowards) * Time.deltaTime);
+    }
+}
