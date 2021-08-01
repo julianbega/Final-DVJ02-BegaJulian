3d2eae82b28a5561d701698c58acbda5f16a8f42
Julian Bega <julian.begamilrad@gmail.com>
2021-08-01 11:56:50 -0300

    Tank allways parallel to floor
    falta doblar y movimiento del cañon para tener el movimiento del player completo

--- FinalDesarrollo2Tanques/Assets/Scripts/PlayerController.cs
+++ FinalDesarrollo2Tanques/Assets/Scripts/PlayerController.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+//[RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(PlayerInputs))]
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody playerRB;
+   // private Rigidbody playerRB;
     private PlayerInputs input;
     [Header("Player speeds")]
     public float speed;
@@ -15,30 +15,29 @@ public class PlayerController : MonoBehaviour
     public float height;
     void Start()
     {
-        playerRB = GetComponent<Rigidbody>();
+       // playerRB = GetComponent<Rigidbody>();
         input = GetComponent<PlayerInputs>();
     }
 
     void Update()
     {
         RaycastHit hitInfo;
-        if (Physics.Raycast (transform.position, -transform.up, out hitInfo))
+        if (Physics.Raycast (transform.position, Vector3.down, out hitInfo))
         {
-            transform.up = hitInfo.normal;
-            transform.position = hitInfo.point + hitInfo.normal * height;
+            transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+            transform.position = new Vector3 (this.transform.position.x, hitInfo.point.y + height, this.transform.position.z);
+            Debug.Log(hitInfo.transform.tag);
         }
-    }
-    void FixedUpdate()
-    {
-        HandleMovement();
+
+        Vector3 wantedPos = transform.forward * input.ForwardInput * speed * Time.deltaTime;
+        this.transform.position += new Vector3(wantedPos.x, 0, wantedPos.z);
     }
 
     protected virtual void HandleMovement()
     {
-        Vector3 wantedPos = transform.position + (transform.forward * input.ForwardInput * speed * Time.deltaTime);
-        playerRB.MovePosition(wantedPos);
+       
         
-        if (input.ForwardInput == 1)
+       /* if (input.ForwardInput == 1)
        { 
         Quaternion wantedRotation = transform.rotation * Quaternion.Euler(Vector3.up * (rotationSpeed * input.RotationInput * Time.deltaTime));
         playerRB.MoveRotation(wantedRotation);
@@ -47,6 +46,6 @@ public class PlayerController : MonoBehaviour
         {
             Quaternion wantedRotation = transform.rotation * Quaternion.Euler(Vector3.down * (rotationSpeed * input.RotationInput * Time.deltaTime));
             playerRB.MoveRotation(wantedRotation);
-        }
+        }*/
     }
 }
