dca86f47f5a79fb9e7dfafa1a41a3b9dee5018aa
Julian Bega <julian.begamilrad@gmail.com>
2021-08-03 01:43:57 -0300

    Shoot bomb

--- FinalDesarrollo2Tanques/Assets/Scripts/Bomb.cs
+++ FinalDesarrollo2Tanques/Assets/Scripts/Bomb.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     private Rigidbody rb;
+    private TrailRenderer trail;
     void Start()
     {
         rb = FindObjectOfType<Rigidbody>();
-        rb.AddForce(transform.forward*1000);
+        rb.AddForce(transform.forward*700);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Mathf.Abs(this.transform.position.x) > 400 || Mathf.Abs(this.transform.position.y) > 400 || Mathf.Abs(this.transform.position.z) > 400)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(this.gameObject);
     }
 }
