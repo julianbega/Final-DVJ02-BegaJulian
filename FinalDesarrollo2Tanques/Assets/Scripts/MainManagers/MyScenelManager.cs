1c52a4d730874962cbe6387e899c83c8a3af17a4
Julian Bega <julian.begamilrad@gmail.com>
2021-08-03 12:55:55 -0300

    Basic scene manager

--- FinalDesarrollo2Tanques/Assets/Scripts/MainManagers/MyScenelManager.cs
+++ FinalDesarrollo2Tanques/Assets/Scripts/MainManagers/MyScenelManager.cs
@@ -1,18 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class MyScenelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static MyScenelManager instanceSceneManager;
+
+    public static MyScenelManager Instance { get { return instanceSceneManager; } }
+
+    private void Awake()
     {
-        
+        if (instanceSceneManager != null && instanceSceneManager != this)
+        {
+            Destroy(FindObjectOfType<MyScenelManager>().gameObject);
+        }
+        else
+        {
+            instanceSceneManager = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        DontDestroyOnLoad(this.gameObject);
+    }
+    private void OnDisable()
+    {
+    }
+
+    public void ChangeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
