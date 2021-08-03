c8211f627a24be41b3080fad5af44761bad8bddf
Julian Bega <julian.begamilrad@gmail.com>
2021-08-03 16:12:32 -0300

    Player controller code refactoring

--- FinalDesarrollo2Tanques/Assets/Scripts/PlayerController.cs
+++ FinalDesarrollo2Tanques/Assets/Scripts/PlayerController.cs
@@ -45,29 +45,30 @@ public class PlayerController : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.FromToRotation(transform.up, hitInfo.normal) * transform.rotation, 0.15f);
             transform.position = new Vector3(this.transform.position.x, hitInfo.point.y + height, this.transform.position.z);
         }
+        MoveForward();
+        RotateTank();
+        rotatingCanon = input.ShootInput;
+    }
 
 
+    void LateUpdate()
+    {
+        if (rotatingCanon) rotateCanon();
+    }
+    private void MoveForward()
+    {
         Vector3 wantedPos = transform.forward * input.ForwardInput * speed * Time.deltaTime;
         this.transform.position += new Vector3(wantedPos.x, 0, wantedPos.z);
         if (input.ForwardInput != 0)
         {
             manager.SetDistanceTraveled(manager.GetDistanceTraveled() + (Time.deltaTime * speed));
         }
-
-        transform.Rotate(0.0f, input.RotationInput* Time.deltaTime * rotationSpeed, 0.0f);
-        // transform.rotation = Quaternion.Slerp(transform.rotation, new Quaternion(transform.rotation.x, input.RotationInput * Time.deltaTime * rotationSpeed, transform.rotation.z, transform.rotation.w), 1);
-
-        if (input.ShootInput)
-        {
-            rotatingCanon = true;
-        }
-
     }
-    void LateUpdate()
+    private void RotateTank()
     {
-        if (rotatingCanon) rotateCanon();
+        transform.Rotate(0.0f, input.RotationInput * Time.deltaTime * rotationSpeed, 0.0f);
+        // transform.rotation = Quaternion.Slerp(transform.rotation, new Quaternion(transform.rotation.x, input.RotationInput * Time.deltaTime * rotationSpeed, transform.rotation.z, transform.rotation.w), 1);
     }
-
     private void rotateCanon()
     {
         RaycastHit hit;
@@ -77,13 +78,13 @@ public class PlayerController : MonoBehaviour
             Vector3 direction = hit.point - transform.position;
             // float rotation = Vector3.Angle(this.transform.position, hit.point);
             // Canon.transform.Rotate(0 , rotation, 0);
-           
-                if (Mathf.Abs(direction.x) >= 1 && Mathf.Abs(direction.z) > 1)
-                {
-                    Canon.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), canonRotationSpeed * Time.deltaTime);
-                    shoot();
-                }
-            
+
+            if (Mathf.Abs(direction.x) >= 1 && Mathf.Abs(direction.z) > 1)
+            {
+                Canon.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), canonRotationSpeed * Time.deltaTime);
+                shoot();
+            }
+
             Debug.Log(direction);
         }
         rotatingCanon = false;
