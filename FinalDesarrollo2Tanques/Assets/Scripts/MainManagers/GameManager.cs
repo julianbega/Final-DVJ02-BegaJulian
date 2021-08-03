c02d8b5919d42e40b222454759c8215d60d00adc
Julian Bega <julian.begamilrad@gmail.com>
2021-08-03 13:12:54 -0300

    Basic gameManager

--- FinalDesarrollo2Tanques/Assets/Scripts/MainManagers/GameManager.cs
+++ FinalDesarrollo2Tanques/Assets/Scripts/MainManagers/GameManager.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public static GameManager instanceGameManager;
+    public static GameManager Instance { get { return instanceGameManager; } }
+
+    public PlayerManager actualPlayer;
+    private void Awake()
+    {
+        if (instanceGameManager != null && instanceGameManager != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instanceGameManager = this;
+        }
+    }
     void Start()
     {
-        
+        DontDestroyOnLoad(this.gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Pause()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
     }
 }
