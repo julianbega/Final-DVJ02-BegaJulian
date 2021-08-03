b3e4c500f6851f278c7b4c0c301f3654c174f0f4
Julian Bega <julian.begamilrad@gmail.com>
2021-08-03 13:13:13 -0300

    Manager reference

---
+++ FinalDesarrollo2Tanques/Assets/Scripts/MainManagers/ManagersReference.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagersReference : MonoBehaviour
+{
+    public GameManager referenceManager;
+    public MyScenelManager referenceSceneManager;
+    private void Awake()
+    {
+        referenceManager = null;
+        referenceSceneManager = null;
+    }
+    void Start()
+    {
+        referenceManager = FindObjectOfType<GameManager>();
+        referenceSceneManager = FindObjectOfType<MyScenelManager>();
+    }
+
+    void Update()
+    {
+        if (referenceManager == null)
+        {
+            referenceManager = FindObjectOfType<GameManager>();
+        }
+        if (referenceSceneManager == null)
+        {
+            referenceSceneManager = FindObjectOfType<MyScenelManager>();
+        }
+
+    }
+
+    public void PauseGame()
+    {
+        referenceManager.Pause();
+    }
+
+    public void GoToMenu()
+    {
+        referenceSceneManager.ChangeScene("Menu");
+    }
+    public void GoToGame()
+    {
+        referenceSceneManager.ChangeScene("Game");
+    }
+    public void GoToCredits()
+    {
+        referenceSceneManager.ChangeScene("Credits");
+    }
+    public void ExitGame()
+    {
+        referenceSceneManager.OnClickQuit();
+    }
+
+}
